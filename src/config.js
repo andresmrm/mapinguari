e9947795e4f8f57114167363cf22f0d4ea21f36c
AndresMRM <andres@inventati.org>
2017-07-21 14:04:56 -0300

    avoid total crash on localStorage access error

--- src/config.js
+++ src/config.js
@@ -27,18 +27,24 @@ let defaultConfig = {
 let config = defaultConfig,
     storeName = defaultConfig.localStorageName
 
-if (localStorage[storeName]) {
-    // Loads previous saved configs
-    let restoredConfig = JSON.parse(localStorage[storeName])
-    if (restoredConfig.version == defaultConfig.version) {
-        // restored config is valid, replace default config
-        config = restoredConfig
+try {
+    if (localStorage[storeName]) {
+        // Loads previous saved configs
+        let restoredConfig = JSON.parse(localStorage[storeName])
+        if (restoredConfig.version == defaultConfig.version) {
+            // restored config is valid, replace default config
+            config = restoredConfig
+        }
     }
+} catch (e) {
+    console.log('Failed to access localStorage. Configs won\'t be saved.')
 }
 
 // Saves configs
 config.save = function saveConfig() {
-    localStorage[storeName] = JSON.stringify(config)
+    try {
+        localStorage[storeName] = JSON.stringify(config)
+    } catch (e) {}
 }
 
 export default config
